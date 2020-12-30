ba950be4dc963abcd4a40afb7bd822f752943d42
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-30 07:24:50 +0700

    #3 - Add SocketHandler

---
+++ src/main/java/com/hendisantika/springbootwebrtc2/SocketHandler.java
@@ -0,0 +1,17 @@
+package com.hendisantika.springbootwebrtc2;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-webrtc2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 30/12/20
+ * Time: 07.24
+ */
+@Component
+public class SocketHandler extends TextWebSocketHandler {
+}
