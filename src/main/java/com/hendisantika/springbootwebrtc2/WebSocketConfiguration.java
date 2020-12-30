1766ad91385934204a126eead53c065cd86cd897
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-30 07:24:20 +0700

    #2 - Add WebSocketConfiguration

---
+++ src/main/java/com/hendisantika/springbootwebrtc2/WebSocketConfiguration.java
@@ -0,0 +1,28 @@
+package com.hendisantika.springbootwebrtc2;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+import java.util.logging.SocketHandler;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-webrtc2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 30/12/20
+ * Time: 07.23
+ */
+@Configuration
+@EnableWebSocket
+public class WebSocketConfiguration implements WebSocketConfigurer {
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(new SocketHandler(), "/socket")
+                .setAllowedOrigins("*");
+    }
+}
