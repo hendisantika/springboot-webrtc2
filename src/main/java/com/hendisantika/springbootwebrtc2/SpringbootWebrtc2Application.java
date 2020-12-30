59eb327836cdb0768e6d4ead58905077e3f9f196
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-30 07:23:00 +0700

    first commit

---
+++ src/main/java/com/hendisantika/springbootwebrtc2/SpringbootWebrtc2Application.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootwebrtc2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebrtc2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWebrtc2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springbootwebrtc2/SpringbootWebrtc2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootwebrtc2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebrtc2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
